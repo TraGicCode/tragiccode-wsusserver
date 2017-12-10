791a02d3c4d647b08990d958d17f9f775cb59683
tragiccode <6224270+TraGicCode@users.noreply.github.com>
2017-12-09 21:56:39 -0600

    WIP

--- spec/acceptance/wsusserver_approvalrule_spec.rb
+++ spec/acceptance/wsusserver_approvalrule_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper_acceptance'
 
 describe 'wsusserver_approvalrule' do
-  context 'when creating an approval rule' do
+  context 'when managing an approval rule' do
     context 'with default parameters' do
       before(:all) do
         @approval_rule_name = SecureRandom.hex(10)
@@ -34,84 +34,72 @@ describe 'wsusserver_approvalrule' do
         include_context 'with a puppet resource run'
         puppet_resource_should_show('ensure', 'present')
         puppet_resource_should_show('enabled', 'true')
+        puppet_resource_should_show('rule_id', %r{\d+})
       end
     end
+    context 'with ensure => absent' do
+      before(:all) do
+        @approval_rule_name = SecureRandom.hex(10)
+        @manifest =  <<-MANIFEST
+        class { 'wsusserver':
+          targeting_mode                            => 'Client',
+          trigger_full_synchronization_post_install => false,
+          products                                  => ['SQL Server'],
+          update_languages                          => ['en'],
+          update_classifications                    => ['Critical Updates', 'Security Updates'],
+        }
 
-      # describe command("((Get-WsusServer).GetInstallApprovalRules() | Where-Object { $PSItem.Name -eq '#{@approval_rule_name}'' }).Count -eq 1") do
-      #   its(:stdout) { is_expected.to match %r{true}i }
-      # end
+        wsusserver_approvalrule { '#{@approval_rule_name}':
+          ensure => 'absent',
+        }
+      MANIFEST
+      end
 
-      # describe command("((Get-WsusServer).GetInstallApprovalRules() | Where-Object { $PSItem.Name -eq '#{@approval_rule_name}' }).Enabled") do
-      #   its(:stdout) { is_expected.to match %r{true}i }
-      # end
+      it_behaves_like 'an idempotent resource'
+      
+      context 'when puppet resource is run' do
+        before(:all) do
+          @result = on(default, puppet('resource', 'wsusserver_approvalrule', @approval_rule_name))
+        end
 
-      # # Checking read-only properties have Something expected yet uncontrolled
-      # describe command("((Get-WsusServer).GetInstallApprovalRules() | Where-Object { $PSItem.Name -eq '#{@approval_rule_name}' }).Id") do
-      #   its(:stdout) { is_expected.to match %r{^[0-9]+$} }
-      # end
+        include_context 'with a puppet resource run'
+        puppet_resource_should_show('ensure', 'absent')
+      end
+    end
+  end
+
+  context 'with enabled => false' do
+    before(:all) do
+      @approval_rule_name = SecureRandom.hex(10)
+      @manifest =  <<-MANIFEST
+      class { 'wsusserver':
+        targeting_mode                            => 'Client',
+        trigger_full_synchronization_post_install => false,
+        products                                  => ['SQL Server'],
+        update_languages                          => ['en'],
+        update_classifications                    => ['Critical Updates', 'Security Updates'],
+      }
+
+      wsusserver_approvalrule { '#{@approval_rule_name}':
+        ensure  => 'present',
+        enabled => false,
+      }
+    MANIFEST
     end
 
-  #   context 'with ensure => absent' do
-  #     before(:all) do
-  #       @approval_rule_name = 'Automatic Approval for Security Updates Rule'
-  #     end
-  #     let(:manifest) do
-  #       <<-MANIFEST
-  #           class { 'wsusserver':
-  #             targeting_mode                            => 'Client',
-  #             trigger_full_synchronization_post_install => false,
-  #             products                                  => ['SQL Server'],
-  #             update_languages                          => ['en'],
-  #             update_classifications                    => ['Critical Updates', 'Security Updates'],
-  #           }
+    it_behaves_like 'an idempotent resource'
+    
+    context 'when puppet resource is run' do
+      before(:all) do
+        @result = on(default, puppet('resource', 'wsusserver_approvalrule', @approval_rule_name))
+      end
 
-  #           wsusserver_approvalrule { '#{@approval_rule_name}':
-  #             ensure => 'absent',
-  #           }
-  #         MANIFEST
-  #     end
-
-  #     it_behaves_like 'an idempotent resource'
-
-  #     describe command("((Get-WsusServer).GetInstallApprovalRules() | Where-Object { $PSItem.Name -eq '#{@approval_rule_name}'' }).Count -eq 0") do
-  #       its(:stdout) { is_expected.to match %r{true}i }
-  #     end
-
-  #   end
-
-  #   context 'with enabled => false' do
-  #     before(:all) do
-  #       @approval_rule_name = 'Automatic Approval for Security Updates Rule'
-  #     end
-  #     let(:manifest) do
-  #       <<-MANIFEST
-  #           class { 'wsusserver':
-  #             targeting_mode                            => 'Client',
-  #             trigger_full_synchronization_post_install => false,
-  #             products                                  => ['SQL Server'],
-  #             update_languages                          => ['en'],
-  #             update_classifications                    => ['Critical Updates', 'Security Updates'],
-  #           }
-
-  #           wsusserver_approvalrule { '#{@approval_rule_name}':
-  #             ensure => 'present',
-  #             enabled => false,
-  #           }
-  #         MANIFEST
-  #     end
-
-  #     after(:all) do
-  #       resource('wsusserver_approvalrule', @approval_rule_name, ensure: 'absent')
-  #     end
-
-  #     it_behaves_like 'an idempotent resource'
-
-  #     describe command("((Get-WsusServer).GetInstallApprovalRules() | Where-Object { $PSItem.Name -eq '#{@approval_rule_name}' }).Enabled") do
-  #       its(:stdout) { is_expected.to match %r{false}i }
-  #     end
-  #   end
-  # end
-
+      include_context 'with a puppet resource run'
+      puppet_resource_should_show('ensure', 'present')
+      puppet_resource_should_show('enabled', 'false')
+      puppet_resource_should_show('rule_id', %r{\d+})
+    end
+  end
   # context 'when adding a classification to a rule' do
   #   let(:manifest) do
   #     <<-MANIFEST
@@ -120,7 +108,7 @@ describe 'wsusserver_approvalrule' do
   #           trigger_full_synchronization_post_install => false,
   #           products                                  => ['SQL Server'],
   #           update_languages                          => ['en'],
-  #           update_classifications                    => ['Critical Updates', 'Security Updates'],
+  #           update_classifications                     => ['Critical Updates', 'Security Updates'],
   #         }
   #         wsusserver::computertargetgroup { 'production':
   #           ensure => 'present',
--- spec/support/matchers/puppet_resource_should_show.rb
+++ spec/support/matchers/puppet_resource_should_show.rb
@@ -1,9 +1,12 @@
 # The property named `property_name` should be shown in the output of puppet resource. If a non-nil value is specified, the matcher only
 # matches if the value is presented (quoted, or unquoted)
+# You can also pass a regex type as value and it will which is great for readonly properties that should have certain characteristics
 def puppet_resource_should_show(property_name, value)
     it "should report the correct #{property_name} value" do
       regex = if value.nil?
                 /(#{property_name})(\s*)(=>)(\s*)/
+              elsif(value.is_a?(Regexp))
+                /(#{property_name})(\s*)(=>)(\s*)('#{value}')/
               elsif(value.is_a?(Array))
                 /(#{property_name})(\s*)(=>)(\s*)(\[#{value.sort.map!{ |v| "'#{v}'" }.join(', ')}\])/i
               else
