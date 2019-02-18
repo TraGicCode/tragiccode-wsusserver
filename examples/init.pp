4940622643a0a97a4788c70472f3f52b611c202e
Nathan Giuliani <nathojg@gmail.com>
2019-02-19 08:48:28 +1030

    update example

--- examples/init.pp
+++ examples/init.pp
@@ -1,14 +1,7 @@
-# The baseline for module testing used by Puppet Inc. is that each manifest
-# should have a corresponding test manifest that declares that class or defined
-# type.
 #
-# Tests are then run by using puppet apply --noop (to check for compilation
-# errors and view a log of events) or by fully applying the test in a virtual
-# environment (to compare the resulting system state to the desired state).
+# example config for wsusserver
 #
-# Learn more about module testing here:
-# https://docs.puppet.com/guides/tests_smoke.html
-#
+
 class { 'wsusserver':
   package_ensure                            => 'present',
   include_management_console                => true,
@@ -25,13 +18,8 @@ class { 'wsusserver':
   synchronize_time_of_day                   => '03:00:00', # this is in UTC, 24H Clock
   number_of_synchronizations_per_day        => 1,
   trigger_full_synchronization_post_install => false,
-  #products                                  => [ '*' ],
-  products                                  => [ 'Windows Server 2008',
-                                              'Windows Server 2008 R2',
-                                              'Windows Server 2012',
-                                              'Windows Server 2012 R2',
-                                              'Windows Server 2016',
-                                              'Windows Server 2019' ],
-  product_families                          => ['SQL Server','Developer Tools, Runtimes, and Redistributables'],
+  #products                                  => [ '*' ], # all products
+  products                                  => [ 'Windows Server 2012 R2', 'Windows Server 2016', 'Windows Server 2019' ],
+  product_families                          => ['SQL Server', 'Developer Tools, Runtimes, and Redistributables'],
   update_classifications                    => [ 'Critical Updates', 'Security Updates', 'Updates'],
 }
