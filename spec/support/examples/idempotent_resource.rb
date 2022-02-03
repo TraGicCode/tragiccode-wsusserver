802f2846b2d8cb880f4e733fb2f645985218402e
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2022-02-03 05:46:01 -0600

    Testing github actions (#58)
    Removed old beaker acceptance tests

--- spec/support/examples/idempotent_resource.rb
+++
@@ -1,9 +0,0 @@
-shared_examples 'an idempotent resource' do
-  it 'runs without errors' do
-    apply_manifest(@manifest, catch_failures: true)
-  end
-
-  it 'runs a second time without changes' do
-    apply_manifest(@manifest, catch_changes: true)
-  end
-end
